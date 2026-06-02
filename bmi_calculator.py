938c97b5c0c1c1b80d9842818e4d11bb8cfbee06
Maddie <28madhurima@gmail.com>
2026-06-02 15:34:38 +0100

    Create bmi_calculator.py

---
+++ bmi_calculator.py
@@ -0,0 +1,9 @@
+# Name Printer
+print("=== NAME PRINTER ===")
+
+first = input("First name: ")
+last = input("Last name: ")
+
+print(f"\nHello {first} {last}!")
+print(f"Initials: {first[0]}.{last[0]}.")
+print(f"UPPER: {first.upper()} {last.upper()}")
